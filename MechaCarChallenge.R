e8b89da2c725997cbf97601f40312b289d8d47e9
CrossCreed <mihaianghel1995@gmail.com>
2022-01-15 00:01:08 -0500

    Deliverable 3

--- MechaCarChallenge.R
+++ MechaCarChallenge.R
@@ -15,3 +15,27 @@ total_summary <- susp_coil %>% summarize(Mean = mean(PSI), Median = median(PSI),
 # Write an RScript that creates a lot_summary dataframe using the group_by() and the summarize() functions to group each 
 # manufacturing lot by the mean, median, variance, and standard deviation of the suspension coil's PSI column.
 lot_summary <- susp_coil %>% group_by(Manufacturing_Lot) %>% summarize(Mean = mean(PSI), Median = median(PSI),Variance = var(PSI), SD = sd(PSI), Manuf_lot=n(), .groups = 'keep')
+
+# Deliverable 3
+# In your MechaCarChallenge.RScript, write an RScript using the t.test() function to determine if the PSI across all manufacturing 
+# lots is statistically different from the population mean of 1,500 pounds per square inch.
+t.test(susp_coil$PSI,mu = 1500)
+# Next, write three more RScripts in your MechaCarChallenge.RScript using the t.test() function and its subset() argument to determine 
+# if the PSI for each manufacturing lot is statistically different from the population mean of 1,500 pounds per square inch.
+lot_one = subset(susp_coil, Manufacturing_Lot == 'Lot1')
+lot_two = subset(susp_coil, Manufacturing_Lot == 'Lot2')
+lot_three = subset(susp_coil, Manufacturing_Lot == 'Lot3')
+
+t.test(lot_one$PSI,mu=1500)
+# data:  lot_one$PSI
+# t = 0, df = 49, p-value = 1
+# alternative hypothesis: true mean is not equal to 1500
+t.test(lot_two$PSI,mu=1500)
+# data:  lot_two$PSI
+# t = 0.51745, df = 49, p-value = 0.6072
+# alternative hypothesis: true mean is not equal to 1500
+t.test(lot_three$PSI,mu=1500)
+# data:  lot_three$PSI
+# t = -2.0916, df = 49, p-value = 0.04168
+# alternative hypothesis: true mean is not equal to 1500
+
