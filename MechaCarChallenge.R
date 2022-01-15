54a9938833d820efad34f1fde84261235b96878f
Mihai Anghel <89520192+CrossCreed@users.noreply.github.com>
2022-01-14 23:43:06 -0500

    Delete MechaCarChallenge.R

--- MechaCarChallenge.R
+++
@@ -1,7 +0,0 @@
-# Deliverable 1
-# Import and read in the MechaCar_mpg.csv
-mechacar_mpg <- read.csv(file='MechaCar_mpg.csv', check.names = F, stringsAsFactors = F)
-# Perform linear regression using the lm() function. In the lm() function, pass in all six variables (i.e.,columns), add the dataframe you created in Step 4 as the data pearameter.
-lm(mpg ~ vehicle_length + vehicle_weight + spoiler_angle + ground_clearance + AWD, mechacar_mpg)
-# Using the summary() function, determine the p-value and the r-squared value for the linear regression model.
-summary(lm(mpg ~ vehicle_length + vehicle_weight + spoiler_angle + ground_clearance + AWD, data=mechacar_mpg)) 
